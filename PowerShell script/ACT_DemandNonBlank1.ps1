e9f350bae79029d84d9c50a6545e1376fab666a8
UshaHorapeti <54103507+UshaHorapeti@users.noreply.github.com>
2026-02-20 21:00:41 +0530

    Ensure connection is closed after exporting results
    Added connection close statement to ensure proper resource management.

--- PowerShell script/ACT_DemandNonBlank1.ps1
+++ PowerShell script/ACT_DemandNonBlank1.ps1
@@ -4,9 +4,9 @@ $adomd_obj = Add-Type -Path $assemblyPath -PassThru
 $Connection_class = $adomd_obj | Where-Object {$_.FullName -eq 'Microsoft.AnalysisServices.AdomdClient.AdomdConnection'}
 $Adapter_class = $adomd_obj | Where-Object {$_.FullName -eq 'Microsoft.AnalysisServices.AdomdClient.AdomdDataAdapter'}
 
-$PowerBILogin = "ISCBI001@bd.com"
-$PowerBIPassword = "52SQ6I>*P+*63*Z"
-$PowerBIEndpoint = "powerbi://api.powerbi.com/v1.0/myorg/E2E%20Supply%20Chain%20Excellence"
+$PowerBILogin = "MailIF"
+$PowerBIPassword = "Password"
+$PowerBIEndpoint = "powerbi://api.powerbi.com/v1.0/myorg/WorkspaceName"
 $reportName = "Demand Reconciliation"
 
 $RunDate = Get-Date
@@ -80,4 +80,5 @@ $Results | export-csv -Path $ExportPath  -NoTypeInformation
 #$Results | Format-Table
 
 $Connection.Dispose()
+
 $Connection.Close()
